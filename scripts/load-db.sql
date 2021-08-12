aa877bdb8d614aafebd80d20b42b9dbb4d6b5a41
Matt Heindel <mheindel94@gmail.com>
2021-08-11 21:19:06 -0700

    Seed database with multiple files in one command

--- scripts/load-db.sql
+++
@@ -1,4 +0,0 @@
-copy styles(id, product_id, name, sale_price, original_price, default_style)
-from '/home/mc_heindel/HackReactor/Products/csv-extracts/styles.csv'
-delimiter ','
-csv header;
---
+++ scripts/seed.sql
@@ -0,0 +1,14 @@
+copy styles
+from '/home/mc_heindel/HackReactor/Products/csv-extracts/styles.csv'
+delimiter ','
+csv header;
+
+copy features
+from '/home/mc_heindel/HackReactor/Products/csv-extracts/features.csv'
+delimiter ','
+csv header;
+
+copy related
+from '/home/mc_heindel/HackReactor/Products/csv-extracts/related.csv'
+delimiter ','
+csv header;
