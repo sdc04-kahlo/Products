1b3e064e18e962049926213a7baaca896cf4ed54
Matt Heindel <mheindel94@gmail.com>
2021-08-12 17:01:41 -0700

    Initialize routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/products', (req, res) => {
+  res.json('hello from products');
+});
+
+app.get('/products/:product_id', (req, res) => {
+  res.json(req.params);
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  res.json(req.params);
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  res.json(req.params);
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
